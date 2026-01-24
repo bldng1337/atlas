a97d4aeb08e1497dd2aa9522acd72a48b3b3b76b
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2026-01-24 08:29:37 +0100

    reduce batch size

--- train_remote.sh
+++ train_remote.sh
@@ -4,7 +4,7 @@ uv run accelerate launch --mixed_precision="bf16" train_t2i.py \
     --dataset_path "bldng/atlas" \
     --mesa_path "NewtNewt/MESA" \
     --output_dir "./outputs/run_5090_uv" \
-    --train_batch_size 12 \
+    --train_batch_size 8 \
     --gradient_accumulation_steps 2 \
     --learning_rate 1e-4 \
     --lr_warmup_steps 500 \
